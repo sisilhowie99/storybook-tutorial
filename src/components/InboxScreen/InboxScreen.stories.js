327261cfd99a533bab999408e741ffbcd760b536
sisilhowie99 <sisil.anastasia@gmail.com>
2022-07-29 01:12:51 +1000

    Add interaction tests
    * Add play function to InboxScreen
    * Automate interaction tests using @storybook/test-runner
    * Add new script to run test runner

--- src/components/InboxScreen/InboxScreen.stories.js
+++ src/components/InboxScreen/InboxScreen.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { rest } from "msw";
+import { fireEvent, within, waitFor, waitForElementToBeRemoved } from "@storybook/testing-library";
 import InboxScreen from "./InboxScreen";
 import store from "../../lib/store";
 import { MockedState } from "../TaskList/TaskList.stories";
@@ -33,4 +34,19 @@ Error.parameters = {
 			}),
 		],
 	},
+};
+
+Default.play = async ({ canvasElement }) => {
+	const canvas = within(canvasElement);
+
+	// Waits for the component to transition from the loading state
+	await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+    
+	// Waits for the component to be updated based on the store
+	await waitFor(async () => {
+		// Simulates pinning the first task
+		await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+		// Simulates pinning the third task
+		await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+	});
 };
